37c20c910e3a53f398d570190ce945867b76b39a
Nicolò Rosini <niko98.nr@gmail.com>
2018-07-15 17:47:29 +0200

    Aggiunti altri layout e activity

--- app/src/main/java/com/example/nicolo/crocerossa/Activity_registrazione.java
+++ app/src/main/java/com/example/nicolo/crocerossa/Activity_registrazione.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_registrazione extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_registrazione);
     }
 
     public void switchViews(View view) {
-        startActivity(new Intent(MainActivity.this, Activity_registrazione.class));
+        startActivity(new Intent(Activity_registrazione.this , Quest_activity.class));
     }
 }
---
+++ app/src/main/java/com/example/nicolo/crocerossa/Quest_activity.java
@@ -0,0 +1,13 @@
+package com.example.nicolo.crocerossa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Quest_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest);
+    }
+}

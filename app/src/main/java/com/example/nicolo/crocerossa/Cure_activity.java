651ef216c4cb3f3d40a66aabfc0a001d10f0ad75
giovanni <Giovalucaroni2@gmail.com>
2018-08-10 22:09:10 +0200

    added Cure_activity to manifest

--- app/src/main/java/com/example/nicolo/crocerossa/Cure_activity.java
+++ app/src/main/java/com/example/nicolo/crocerossa/Cure_activity.java
@@ -1,12 +1,20 @@
 package com.example.nicolo.crocerossa;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Cure_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(Color.WHITE);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        }
         setContentView(R.layout.activity_cure);
         setTitle("Gestione Farmaci e scadenze");
     }
--- app/src/main/java/com/example/nicolo/crocerossa/Quest_activity.java
+++ app/src/main/java/com/example/nicolo/crocerossa/Quest_activity.java
@@ -1,6 +1,8 @@
 package com.example.nicolo.crocerossa;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +19,11 @@ public class Quest_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(Color.WHITE);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        }
         setContentView(R.layout.activity_quest);
         setTitle("Questionario anamnestico");
     }
